abbedaf993ad4f0712cd7d3b2b56723504891e8d
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-18 15:23:05 +0200

    fixed test

--- backend/src/test/java/service/CalculationServiceTest.java
+++ backend/src/test/java/service/CalculationServiceTest.java
@@ -10,7 +10,7 @@ class CalculationServiceTest {
     void testAddConditional() {
         CalculationService calculationService = new CalculationService();
         Integer actual = calculationService.addConditional(1, 2);
-        assertTrue(actual == 3);
+        assertTrue(1==actual);
     }
 
 }
