fc83abe20c5cd37d28187b4c54ec4ca75b6d75ff
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-18 15:18:39 +0200

    added missing test annotation

--- backend/src/test/java/service/CalculationServiceTest.java
+++ backend/src/test/java/service/CalculationServiceTest.java
@@ -1,9 +1,12 @@
 package service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationServiceTest {
 
+    @Test
     void testAddConditional() {
         CalculationService calculationService = new CalculationService();
         Integer actual = calculationService.addConditional(1, 2);
